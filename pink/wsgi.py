02cde8bb3587eda36178d5c67a7f648cd5a794a1
jhon-palma <jhon-palma@upc.edu.co>
2016-06-01 19:00:40 -0500

    actualizo vista para el admin

--- pink/wsgi.py
+++ pink/wsgi.py
@@ -7,18 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
 #modo desarrollo 
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pink.settings")
-
-application = get_wsgi_application()
-#modo produccion
-#import os
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pink.settings")
 #from django.core.wsgi import get_wsgi_application
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pink.settings")
+
+#application = get_wsgi_application()
+#modo produccion
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pink.settings")
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+application = Cling(get_wsgi_application())
